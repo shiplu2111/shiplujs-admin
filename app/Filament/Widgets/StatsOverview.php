6438674f1ccb3f7aa041db3ef78add7c2cbd3357
Md Enzamamul Haque Shiplu <shiplu2111@gmail.com>
2025-05-22 17:56:34 +0600

    widget added

---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use App\Models\Social;
+class StatsOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+           Stat::make('Social', Social::count() )
+            ->description('Social Media Accounts')
+            ->descriptionIcon('heroicon-m-arrow-trending-up')
+            ->color('success'),
+        Stat::make('Bounce rate', '21%')
+            ->description('7% increase')
+            ->descriptionIcon('heroicon-m-arrow-trending-down')
+            ->color('danger'),
+        Stat::make('Average time on page', '3:12')
+            ->description('3% increase')
+            ->descriptionIcon('heroicon-m-arrow-trending-up')
+            ->color('success'),
+
+        ];
+    }
+}
